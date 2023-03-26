426489121c11576c55bae5d7b97ee45beb4dba4f
pmsoto <priscillamsoto17@gmail.com>
2023-03-26 14:55:07 -0700

    added axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import Weather from "./Weather.js";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return root.render(
+    <StrictMode>
+      <div className="App">
+        <h1>Weather Search Engine</h1>
+        <Weather />
+      </div>
+    </StrictMode>
   );
 }
 
